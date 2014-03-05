95520653fcf6d699280a51b5bedaf4e4544b7bb0
Liupo Wu <wuliupo@163.com>
2014-03-05 16:00:26 +0800

    copy site from http://mp.weixin.qq.com/wiki

---
+++ js/loadcontent.js
@@ -0,0 +1,39 @@
+$(function(){
+    $('a[href^="#"]').live('click', function(e){
+        this.href='#'+this.innerHTML;
+        loadContent(this.innerHTML);
+    });
+
+    var portals=$(".portal").each(function(){
+        var portal = $(this).click(function(){
+            portals.removeClass('active');
+            $(this).addClass('active');
+        });
+        var as = portal.find('a').data('portal', portal);
+    });
+    var navs=portals.find('a');
+    var firstHeading=$('#firstHeading');
+    var bodyContent=$('#bodyContent');
+    var placeHolder=bodyContent.html();
+    loadContent(location.hash);
+    
+    function loadContent(title){
+        title=!title || title.length<2 ? '%E9%A6%96%E9%A1%B5' : title.replace('#', '');
+        bodyContent.html(placeHolder);
+        $.get('htm/'+title+'.htm', function(content){
+            bodyContent.html(content);
+            title=decodeURIComponent(title);
+            setNav(title);
+            firstHeading.html(title);
+        });
+    }
+    
+    function setNav(hash){
+        navs.each(function(){
+            var a = $(this);
+            if (hash == a.html()){
+                a.addClass("selected").data('portal').click();
+            }
+        });
+    }
+});
